48d85d914ef49861e3579154dc616d855fea12cd
EMUNELE <52833663+EMUNELE@users.noreply.github.com>
2020-11-12 12:48:36 +0400

    Updates

---
+++ precache-manifest.8065a1d0036f6d14c0baaba75334aaec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "346842d89273cc10e402d4fbe4f2ec8c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4a1bc660abe40a3ff63c",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "51bf97c478528eee2645",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "4a1bc660abe40a3ff63c",
+    "url": "./static/js/2.d3d2d3a5.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.d3d2d3a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51bf97c478528eee2645",
+    "url": "./static/js/main.a9613de8.chunk.js"
+  },
+  {
+    "revision": "8e2d2c86df6eeb9f221a",
+    "url": "./static/js/runtime-main.96d449f6.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.51f03adaf40e3c543ed416af9b10328a.js"
+  "./precache-manifest.8065a1d0036f6d14c0baaba75334aaec.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
