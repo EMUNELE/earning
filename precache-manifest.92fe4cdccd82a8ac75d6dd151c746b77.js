2e06ecf6a6c5ae3196bfbb0986eba91422bed994
EMUNELE <52833663+EMUNELE@users.noreply.github.com>
2020-11-12 13:16:15 +0400

    Updates

--- precache-manifest.92fe4cdccd82a8ac75d6dd151c746b77.js
+++ precache-manifest.92fe4cdccd82a8ac75d6dd151c746b77.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "18c0fb25bf8df1cb022c73560644b0d6",
+    "revision": "cf8707be95d709ddc3f87c91e2661f02",
     "url": "./index.html"
   },
   {
-    "revision": "4a1bc660abe40a3ff63c",
+    "revision": "550a6b3fb91517e2129a",
     "url": "./static/css/2.86df01db.chunk.css"
   },
   {
-    "revision": "28153a3fddcb7953c212",
+    "revision": "d150a738efda694cf43c",
     "url": "./static/css/main.981f7211.chunk.css"
   },
   {
-    "revision": "4a1bc660abe40a3ff63c",
-    "url": "./static/js/2.d3d2d3a5.chunk.js"
+    "revision": "550a6b3fb91517e2129a",
+    "url": "./static/js/2.b5ee0e07.chunk.js"
   },
   {
     "revision": "21ced859ea2b2d6b856d461ad6c2afed",
-    "url": "./static/js/2.d3d2d3a5.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.b5ee0e07.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "28153a3fddcb7953c212",
-    "url": "./static/js/main.d238aa9c.chunk.js"
+    "revision": "d150a738efda694cf43c",
+    "url": "./static/js/main.476b62d3.chunk.js"
   },
   {
     "revision": "8e2d2c86df6eeb9f221a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.99c65ee765093335bdbafbadc22abe8a.js"
+  "./precache-manifest.92fe4cdccd82a8ac75d6dd151c746b77.js"
 );
 
 self.addEventListener('message', (event) => {
